e8e9a04a34dcbf9f6ac7ac23148bdf6a869ee896
Javi <javier.marengo@devbase.us>
2024-02-29 18:21:43 -0300

    feat: add infinite scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Typography, Box } from "@mui/material";
+import { useInView } from "react-intersection-observer";
 import ProfileImage from "./assets/profile2.png";
 import HomeLayout from "./layouts/HomeLayout";
 import Header from "./components/Header/Header";
@@ -9,6 +10,12 @@ import { SearchIcon } from "./assets/icons";
 import PokemonSection from "./sections/PokemonSection";
 
 function App() {
+  const { ref, inView } = useInView();
+  const [fetchNextPage, setFetchNextPage] = useState(false);
+  useEffect(() => {
+    setFetchNextPage(inView);
+  }, [inView]);
+
   return (
     <HomeLayout>
       <Box mb="30px">
@@ -45,8 +52,9 @@ function App() {
       </Box>
       <Box mb="30px">Pokemon kinds</Box>
       <Box mb="30px">
-        <PokemonSection />
+        <PokemonSection fetchNextPage={fetchNextPage} />
       </Box>
+      <Box ref={ref} />
     </HomeLayout>
   );
 }
--- src/sections/PokemonSection.jsx
+++ src/sections/PokemonSection.jsx
@@ -1,21 +1,44 @@
-import React from "react";
-import { useQuery } from "@tanstack/react-query";
+import React, { useState, useEffect } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { Grid } from "@mui/material";
 import fetchPokemonList from "../services/fetchPokemonList";
 import Pokemon from "../components/Pokemon/Pokemon";
 
-function PokemonSection() {
-  const { data, isLoading, error } = useQuery({
+function PokemonSection({ fetchNextPage }) {
+  const [pokemons, setPokemons] = useState([]);
+  const {
+    data,
+    isLoading,
+    error,
+    fetchNextPage: fetchNextPokemons,
+  } = useInfiniteQuery({
     queryKey: ["pokemonList"],
-    queryFn: fetchPokemonList,
+    queryFn: ({ pageParam }) => fetchPokemonList({ page: pageParam }),
+    initialPageParam: 1,
+    getNextPageParam: (_lastPage, _allPages, lastPageParam) =>
+      lastPageParam + 1,
   });
+
+  useEffect(() => {
+    if (data?.pages) {
+      setPokemons(data?.pages.flatMap((poke) => poke.results));
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (fetchNextPage) {
+      fetchNextPokemons();
+    }
+  }, [fetchNextPage]);
+
+  console.log(pokemons);
 
   if (isLoading) return "Loading";
   if (error) return "Error";
 
   return (
     <Grid container spacing={2}>
-      {data.results.map((res, index) => (
+      {pokemons.map((res, index) => (
         <Grid item xs={6} md={4} lg={3} key={res.name}>
           <Pokemon name={res.name} url={res.url} index={index} />
         </Grid>
