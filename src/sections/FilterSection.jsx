edff28c6e6510dcdc6f784c86f8445f2d75d3c76
Javi <javier.marengo@devbase.us>
2024-02-29 19:42:31 -0300

    feat: select filter added

--- src/sections/FilterSection.jsx
+++ src/sections/FilterSection.jsx
@@ -1,8 +1,22 @@
-import React from "react";
-import { Box, Button } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
 import pokemonTypes from "../constants/const";
 
 function FilterSection() {
+  const [filtersSelected, setFiltersSelected] = useState([]);
+
+  const toogleFilter = (type) => {
+    const isSelected = filtersSelected.find(
+      (filterType) => filterType === type,
+    );
+    if (isSelected) {
+      setFiltersSelected(
+        filtersSelected.filter((filterType) => filterType !== type),
+      );
+      return;
+    }
+    setFiltersSelected([...filtersSelected, type]);
+  };
   return (
     <Box
       sx={{
@@ -16,14 +30,26 @@ function FilterSection() {
           sx={{
             display: "inline-block",
             textAlign: "center",
-            textDecoration: "none",
-            color: "#ACACAC",
-            padding: "14px",
-            fontSize: "17px",
-            fontWeight: 500,
           }}
+          onClick={() => toogleFilter(type.type)}
+          key={type.type}
         >
-          {type.type}
+          <Typography
+            sx={{
+              fontSize: "17px",
+              fontWeight: 500,
+              color:
+                filtersSelected.indexOf(type.type) === -1 ? "#ACACAC" : "white",
+              textDecoration:
+                filtersSelected.indexOf(type.type) === -1
+                  ? "none"
+                  : "underline",
+              textUnderlineOffset: "6px",
+              textDecorationThickness: "3px",
+            }}
+          >
+            {type.type}
+          </Typography>
         </Button>
       ))}
     </Box>
